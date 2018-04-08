c9670917ee58c105c72e1828df3ace2b559b1765
Hugo Darwood <hugodarwood@gmail.com>
2018-04-08 16:22:55 +0100

    initial work

---
+++ three_word_index.py
@@ -0,0 +1,34 @@
+from nltk.corpus import words
+import re
+from functools import reduce
+import pandas as pd
+import numpy as np
+
+r = re.compile(r'([a-zA-Z]).*?\1|oe|sc|kh')
+
+
+def word_filter(w):
+    if len(w) > 6 or \
+            len(w) < 2 or \
+            any([i == 'x' or i == 'z' for i in w]) or \
+            bool(r.search(w)):
+        return False
+    else:
+        return True
+
+
+def combine_words(l):
+    return reduce((lambda x, y: x + '.' + y), l)
+
+wordlist = [i.lower() for i in words.words()]
+print('total length', len(wordlist))
+filtered_list = [i for i in filter(word_filter, wordlist)]
+print('filtered list lenght', len(filtered_list))
+
+
+
+
+
+
+three_word_index = pd.DataFrame(np.random.choice(filtered_list, (100000, 3), True)).drop_duplicates().apply(
+    combine_words, axis=1)
