3c9dbae87d8467cd9730b869e549f084e52c3b32
Hugo Darwood <hugodarwood@gmail.com>
2018-05-13 11:35:03 +0100

    Updated to use custom vocabs

--- examples/three_word_index.py
+++ examples/three_word_index.py
@@ -1,5 +1,4 @@
 import logging
-import pandas as pd
 
 from human_word_indexes.word_index_generator import WordIndexFilter
 from human_word_indexes.word_index_generator import WordIndexGenerator
@@ -14,4 +13,7 @@ if __name__ == '__main__':
     logger.info(twi.generate_word_index())
     word_filter = WordIndexFilter().set_max_length(5).set_min_length(1)
     twi = WordIndexGenerator(5, word_count=2, word_filter=word_filter)
+    logger.info(twi.generate_word_index())
+    logger.info('Using custom vocabulary')
+    twi = WordIndexGenerator(10,custom_corpus=open('food_words.txt','r').read().split('\n'))
     logger.info(twi.generate_word_index())
